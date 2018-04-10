6c6e927bfa82c26af844fd43b44c685c73fa0c62
darraneteo <biz@darraneteo.com>
2018-04-10 22:21:48 +0100

    Addition of buy one apple get one free. Addition of 3 for 2 two oranges.

--- src/main/scala/HMRC_Start.scala
+++ src/main/scala/HMRC_Start.scala
@@ -30,7 +30,7 @@ object HMRC_Start {
 
         val inputStream = Observable.just()
 
-        val (tillTillElementStream, total) = Till.TillRoll(inputStream, typeMap)
+        val (tillTillElementStream, total, _, _) = Till.TillRoll(inputStream, typeMap)
 
         tillTillElementStream.subscribe(
           x => {
@@ -66,14 +66,14 @@ object HMRC_Start {
 
 
       // Test 2. Single Orange
-      // (Orange, 25, 25.0)
+      // (Orange, 25.0, ,25.0)
       // total = 25.0
       {
         println("Simple Items:Test 2")
 
         val inputStream = Observable.just("Orange")
 
-        val (tillTillElementStream, total) = Till.TillRoll(inputStream, typeMap)
+        val (tillTillElementStream, total, _, _) = Till.TillRoll(inputStream, typeMap)
 
         tillTillElementStream.subscribe(
           x => {
@@ -105,15 +105,61 @@ object HMRC_Start {
 
       }
 
-      // Test 3. Single Apple
+      // Test 2.1 Two for Three Orange
+      // (Orange, 25.0, 25.0)
+      // (Orange, 25.0, 3for2 part 1, 50.0) still charge for 1st orange
+      // (Orange, 0.0, 3for2 part 2 free, 50.0)  do not charge for 3rd orange.
+      // total = 50.0
+
+      // Note tillItem is (type, cost, offer if any, total cost)
+      // but can be changed easily.
+      {
+        println("Simple Items:Test 2")
+
+        val inputStream = Observable.just("Orange","Orange")
+
+        val (tillTillElementStream, total, _, _) = Till.TillRoll(inputStream, typeMap)
+
+        tillTillElementStream.subscribe(
+          x => {
+            println(s"$x")
+          }
+          ,
+          e => {
+            e.printStackTrace()
+          }
+          ,
+          () => {
+            println(s"done")
+          }
+        )
+
+        total.subscribe(
+          x => {
+            println(s"final cost $x")
+          }
+          ,
+          e => {
+            e.printStackTrace()
+          }
+          ,
+          () => {
+            println(s"done")
+          }
+        )
+
+      }
+
+      // Test 3. Single Apple->Two for one
       // (Apple, 60.0, 60.0)
+      // (Apple, 0, this is free, 60.0) // Do not change for this apple.
       // total = 60.0
       {
         println("Simple Items:Test 3")
 
         val inputStream = Observable.just("Apple")
 
-        val (tillTillElementStream, total) = Till.TillRoll(inputStream, typeMap)
+        val (tillTillElementStream, total, _, _) = Till.TillRoll(inputStream, typeMap)
 
         tillTillElementStream.subscribe(
           x => {
@@ -146,18 +192,22 @@ object HMRC_Start {
 
 
       // Test 4. Some random stuff, Apple, Orange, Orange, Apple, Apple.
-      // (Apple, 60.0, 60.0)
-      // (Orange, 25, 85.0)
-      // (Orange, 25, 110.0)
+      // (Apple, 60.0, ,60.0)
+      // (Apple, 0.0, get 2 nd free,60.0) //free
+      // (Orange, 25, ,85.0)
+      // (Orange, 25, 3 for 2 oranges ,110.0)
+      // (Orange, 0.0, get this one free, 110.0)
       // (Apple, 60.0, 170.0)
+      // (Apple, 0.0, free, 17.0)
       // (Apple, 60.0, 230.0)
+      // (Apple, 0.0, free, 230.0)
       // total = 230.0
       {
         println("Simple Items:Test 4")
 
         val inputStream = Observable.just("Apple", "Orange", "Orange", "Apple", "Apple")
 
-        val (tillTillElementStream, total) = Till.TillRoll(inputStream, typeMap)
+        val (tillTillElementStream, total, _, _) = Till.TillRoll(inputStream, typeMap)
 
         tillTillElementStream.subscribe(
           x => {
--- src/main/scala/TillRoll.scala
+++ src/main/scala/TillRoll.scala
@@ -1,21 +1,39 @@
 import rx.lang.scala.Observable
 
+import scala.collection.immutable.HashMap
+
 object Till {
 
   def
   TillRoll(inputStream: Observable[String], typeMap: Map[String, Double])
-  : ( Observable[(String, Double, Double)],  Observable[Double] )
+  : ( Observable[(String, Double, String, Double)],  Observable[Double], Observable[(String, Double, String)], Observable[HashMap[String, Int]] )
   = {
-    val costStream = inputStream.map[Double](x => typeMap(x))
 
-    val totalStream = costStream.scan(0.0d)((a, b) => a + b).drop(1)
+    val groupbyCountOutStream = inputStream
+      .scan(HashMap[String, Int]())((x, y) => x.+(y -> (x.getOrElse(y, 0) + 1))).drop(1)
 
-    val tillTillElementStream = inputStream
-      .zipWith(costStream)({ case (x1, x2) => (x1, x2) })
-      .zipWith(totalStream)({ case ((x1, x2), x3) => (x1, x2, x3) })
+    val itemCostStream = inputStream.map(x => (x, typeMap(x)))
 
-    val total = tillTillElementStream.map({ case (x1, x2, x3) => x3 }).lastOrElse(0.0d)
-    ( tillTillElementStream, total)
+    val costStreamPostOffer = itemCostStream.zip(groupbyCountOutStream)
+      .map({ case (a, x) =>
+        if (a._1 == "Apple")
+          Observable.just[(String, Double, String)]((a._1, a._2, "so_bogof_1"), (a._1, 0.0d, "so_bogof_2_free")) // special offer buy one get one free
+        else if (a._1 == "Orange")
+          if (((x.get(a._1).get) % 2) == 0)
+            Observable.just[(String, Double, String)]((a._1, a._2, "so_3for2_1 " + (x.get(a._1).get)), (a._1, 0.0d, "so_3for2_2_free"))
+          else
+            Observable.just[(String, Double, String)]((a._1, a._2, " " + (x.get(a._1).get)))
+        else
+          Observable.just[(String, Double, String)]((a._1, a._2, ""))
+      }).flatten
+
+    val subTotalStream = costStreamPostOffer.map(x => x._2).scan(0.0d)((a, b) => a + b).drop(1)
+
+    val tillElementStream = Observable.zip(
+      costStreamPostOffer.map(x => x._1), costStreamPostOffer.map(x => x._2), costStreamPostOffer.map(x => x._3), subTotalStream)
+
+    val total = tillElementStream.map({ case (x1, x2, x3, x4) => x4 }).lastOrElse(0.0d)
+    ( tillElementStream, total, costStreamPostOffer, groupbyCountOutStream)
   }
 
 }
